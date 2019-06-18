a5fb7efab25a63d6b973a85129a74f4465f39e62
AtteMa <martikainen.atte@gmail.com>
2019-06-18 18:33:26 +0300

    osa2

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -40,31 +40,44 @@ const Part = (props) => {
   )
 }
 
+const Course = (props) => props.course.parts.map(part =>
+  <li key={part.id}>
+    {part.name} {part.exercises}
+  </li>
+)
+
+
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
-        
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'extra part',
+        exercises: 500,
+        id: 4
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   )
 }
